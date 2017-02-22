b7beae4381f523209c5f416792e2d139aa28695d
sherif20766 <sherif20766@hotmail.com>
2017-02-22 14:34:54 +0200

    added descriptions

--- 01.Hello_World.js
+++ 01.Hello_World.js
@@ -1,3 +1,10 @@
+/*Create an Express.js app that outputs "Hello World!" when somebody goes to /home.
+
+The port number will be provided to you by expressworks as the first argument of
+
+the application, i.e., process.argv[2].*/
+
+
 const express = require("express");
 const app = express();
 
--- 02.Static.js
+++ 02.Static.js
@@ -1,3 +1,12 @@
+/*This exercise is about serving static assets like HTML files.
+
+There are many ways to do it, but we want you to apply static middleware to serve the file index.html.
+
+Please don't use ANY routes like app.get. ONLY static.
+
+Your solution must listen on the port number supplied by process.argv[2].*/
+
+
 const path = require("path");
 const express = require("express");
 const app = express();
